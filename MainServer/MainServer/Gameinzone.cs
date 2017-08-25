8609a40713833914c6bbb0a0c49405444df6bbaa
Storm-x <storm@skif.by>
2017-08-25 17:53:12 +0300

    Storm-x

--- MainServer/MainServer/Gameinzone.cs
+++ MainServer/MainServer/Gameinzone.cs
@@ -397,7 +397,7 @@ namespace MainServer
 
                         deadLine = DateTime.Now.AddSeconds(25);
                         gm.step = 3;
-                        gm.Cell = 0; // rn.rnd();
+                        gm.Cell = rn.rnd();
                         //if (gm.Cell == 0) gm.Cell = 1;
                         /* if (gm.Cell == 0)
                          {
